c7c96025775af195d5f409d8b2c8e94a35ddc9a5
jeetkanodia <jeetkanodia@gmail.com>
2023-09-07 04:07:09 +0530

    deployed

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,9 @@
 import TopicsList from "@/components/TopicsList";
 
 export default function Home() {
-  //  return (<><TopicsList /></>);
-  return <div>Hello</div>;
+  return (
+    <>
+      <TopicsList />
+    </>
+  );
 }
